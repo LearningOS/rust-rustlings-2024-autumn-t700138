e0677b7f148a7be495f95cbffc18db0b6c2397ed
ShidaYu <oslab@ShidaYu.localdomain>
2024-10-16 17:14:51 +0800

    update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -29,12 +29,18 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        let mut a=true;
+        if self.sender_country == self.recipient_country {
+            a = false;
+        }
+        a
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+        self.weight_in_grams * cents_per_gram
     }
 }
 
